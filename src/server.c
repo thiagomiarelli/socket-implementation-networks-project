971d77f6280c67f0de49bd4199e44996f9650436
Thiago Miarelli <thiagomiarelli@gmail.com>
2023-05-13 09:00:16 -0300

    breaks content

--- src/server.c
+++ src/server.c
@@ -13,6 +13,49 @@
 #define MAX_FILESIZE 512
 #define MAX_COMMAND_SIZE 256
 
+/* ==== AUX FUNCTIONS ==== */
+void usage(int argc, char *argv[]);
+int setup_server(int argc, char* argv[]);
+int handle_client_conections(int sockfd);
+int break_filename_and_content(char* message, char* filename, char* content);
+int check_if_end(char* message, int i);
+
+
+int main(int argc, char *argv[]) {
+
+    int sockfd = setup_server(argc, argv);
+    int clientfd = handle_client_conections(sockfd);
+
+    /* ====== ACCEPTING CONNECTIONS ====== */
+    while(1){
+
+        char message[MAX_FILESIZE];
+        memset(message, 0, MAX_FILESIZE);
+
+        if(receiveMessage(message, clientfd) < 0) logexit("receiveFile");
+
+        char filename[MAX_FILESIZE];
+        char content[MAX_FILESIZE];
+
+        if(break_filename_and_content(message, filename, content) < 0) logexit("break_filename_and_content");
+
+        printf("Filename: %s\n", filename);
+        printf("Content: %s\n", content);
+
+        printf("Received: %s\n", message);
+
+        char acknolegment[MAX_FILESIZE];
+
+        sprintf(acknolegment, "[LOG] Acknoledgement from: %.512s\n", message);
+        
+        if(sendMessage(acknolegment, clientfd) < 0) logexit("sendFile");
+    }
+    close(clientfd);
+
+
+    exit(EXIT_SUCCESS);
+}
+
 
 void usage(int argc, char *argv[]) {
     printf("Usage: %s <v4|v6> <server port>\n", argv[0]);
@@ -59,29 +102,29 @@ int handle_client_conections(int sockfd){
     return clientfd;
 }
 
-int main(int argc, char *argv[]) {
-
-    int sockfd = setup_server(argc, argv);
-    int clientfd = handle_client_conections(sockfd);
-
-    /* ====== ACCEPTING CONNECTIONS ====== */
-    while(1){
-
-        char message[MAX_FILESIZE];
-        memset(message, 0, MAX_FILESIZE);
-
-        if(receiveMessage(message, clientfd) < 0) logexit("receiveFile");
-
-        printf("Received: %s\n", message);
-
-        char acknolegment[MAX_FILESIZE];
-
-        sprintf(acknolegment, "[LOG] Acknoledgement from: %.512s\n", message);
-        
-        if(sendMessage(acknolegment, clientfd) < 0) logexit("sendFile");
+int break_filename_and_content(char* message, char* filename, char* content){
+    int i = 0;
+    while(message[i] != '\n'){
+        filename[i] = message[i];
+        i++;
     }
-    close(clientfd);
+    filename[i] = '\0';
+    i++;
 
+    if(strlen(message) - i < 5) return -1; // if there is no space to store the /end
+    int j = 0;
 
-    exit(EXIT_SUCCESS);
+    while(!check_if_end(message, i + 1)){
+        content[j] = message[i];
+        i++;
+        j++;
+    }
+    content[j] = '\0';
+
+    return 0;
+}
+
+int check_if_end(char* message, int i){
+    if(message[i] == '\\' && message[i+1] == 'e' && message[i+2] == 'n' && message[i+3] == 'd') return 1;
+    return 0;
 }
